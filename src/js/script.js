43a2d5374294a3daeb50765e9f313acca1ba02cd
Adil <Adil523@mail.ru>
2020-11-22 16:43:55 +0600

    Commit ready develop ws

---
+++ src/js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    $('.carousel_inner').slick({
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icon/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icon/right.png"></button>',
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                arrows: false,
+                dots: true,
+            }
+        }]
+    });
+    $('ul.catalog_item ').on('click', 'li:not(.catalog_link_active)', function() {
+        $(this)
+          .addClass('catalog_link_active').siblings().removeClass('catalog_link_active')
+          .closest('div.container').find('div.catalog_tabs').removeClass('catalog_tabs_active').eq($(this).index()).addClass('catalog_tabs_active');
+      });
+      $('.content_details').each(function(i) {
+          $(this).on('click', function(e){
+              e.preventDefault();
+              $('.content_wrapper_item').eq(i).toggleClass('content_wrapper_item_active'),
+              $('.content_wrapper_list').eq(i).toggleClass('content_wrapper_list_active');
+          })
+      })
+      
+      $('.catalog_back').each(function(i) {
+        $(this).on('click', function(e){
+            e.preventDefault();
+            $('.content_wrapper_item').eq(i).toggleClass('content_wrapper_item_active'),
+            $('.content_wrapper_list').eq(i).toggleClass('content_wrapper_list_active');
+        })
+    })
+})
